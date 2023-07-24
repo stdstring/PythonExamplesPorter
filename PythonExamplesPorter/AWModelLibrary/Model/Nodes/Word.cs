3e43ad1ace40275720d130ce308f6de5e6868fc1
stdstring <std_string@mail.ru>
2023-07-24 22:33:00 +0500

    added model library for integration tests

---
+++ PythonExamplesPorter/AWModelLibrary/Model/Nodes/Word.cs
@@ -0,0 +1,16 @@
+﻿namespace AWords
+{
+    public class Word : Node
+    {
+        public Word(String text) : base(new List<Node>())
+        {
+            _text = text;
+        }
+
+        public override NodeType NodeType => NodeType.Word;
+
+        public override String GetText() => _text;
+
+        private readonly String _text;
+    }
+}
